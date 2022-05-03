725bd63b650e648ec263ba19aa52811624b77400
ayashiau-ga <angelicashiau@gmail.com>
2022-05-03 13:40:28 -0700

    commit

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('events/create/', views.EventCreate.as_view(), name='events_create'),
     path('events/', views.events_index, name='events'),
     path('events/<int:event_id>/', views.event_detail, name='event_detail'),
-    path('events/<str:event_id>/create', views.ticketmaster_create, name='ticketmaster_create'),
+    path('events/<str:event_id>/delete', views.ticketmaster_create, name='ticketmaster_create'),
     path('events/<int:event_id>/create_comment', views.create_comment, name="create_comment"),
     path('events/<int:event_id>/delete/<int:comment_id>', views.delete_comment, name="delete_comment"),
     path('events/search/', views.search, name="search"),
@@ -17,6 +17,4 @@ urlpatterns = [
     path('user/create', views.create_user, name='create_user'),
     path('user/add_bio/<int:user_id>', views.add_bio, name='add_bio'),
     path('user/<int:user_id>/not_going/<int:event_id>', views.not_going, name='not_going'),
-    path('events/<int:event_id>/update/<int:comment_id>', views.update_comment, name='update_comment'),
-    path('events/<int:event_id>/update_content/<int:comment_id>', views.update_content, name='update_content'),
 ]
