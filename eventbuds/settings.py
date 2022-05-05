aaba9b070f3a1a63f9fcd4e5716aed7e06f0d598
ayashiau-ga <angelicashiau@gmail.com>
2022-05-05 10:48:03 -0700

    settings updated

--- eventbuds/settings.py
+++ eventbuds/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
-# Configure Django App for Heroku.
-import django_on_heroku
-django_on_heroku.settings(locals())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 key = load_dotenv('SECRET_KEY')
@@ -128,3 +126,7 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
